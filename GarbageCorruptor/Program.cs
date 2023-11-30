a4ea0e23ffd2a88ce46d5201afe1c5b5e87484cb
cetio <qubbinyt@gmail.com>
2023-11-29 21:41:59 -0500

    Added better heap corruption!

--- GarbageCorruptor/Program.cs
+++ GarbageCorruptor/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Linq.Expressions;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace GarbageCorruptor
 {
@@ -6,9 +9,45 @@ namespace GarbageCorruptor
     {
         static unsafe void Main(string[] args)
         {
-            int[] arr = new int[1];
-            *(int*)Unsafe.AsPointer(ref Unsafe.As<RelaxedGrossObject>(arr).Data) = int.MaxValue;
-            GC.Collect();
+            string str = "Hello world!";
+            CopyString(str, "GRAAAAAAAAAHHHHHHHHHHH");
+            Console.WriteLine(str); // GRAAAAAAAAAHHHHHHHHHHH
+
+            Array array = AllocateArray(1, typeof(void).MakeArrayType());
+            Console.WriteLine(array.Length); // 1
+            Console.WriteLine(array); // System.Void[]
+
+            ResizeArray(int.MaxValue, array);
+            Console.WriteLine(array.Length); // 2147483647
+
+            GC.Collect(); // Fatal error. Internal CLR error. (0x80131506)
+        }
+
+        public static unsafe void CopyString(string str1, string str2)
+        {
+            *(int*)Unsafe.AsPointer(ref Unsafe.As<RelaxedGrossObject>(str1).Data) = str2.Length;
+            char* chptr = (char*)Unsafe.AsPointer(ref Unsafe.As<RelaxedGrossObject>(str1).Data) + 2;
+
+            for (int i = 0; i < str2.Length; i++)
+                chptr[i] = str2[i];
+        }
+
+        public static unsafe dynamic AllocateArray(int length, Type type)
+        {
+            if (!type.HasElementType)
+                return null!;
+
+            byte[] bytes = new byte[Marshal.SizeOf(type.GetElementType()!) * length];
+            ResizeArray(length, bytes);
+            *((nint*)Unsafe.AsPointer(ref Unsafe.As<RelaxedGrossObject>(bytes).Data) - 1) = type.TypeHandle.Value;
+
+            return bytes;
+        }
+
+        public static unsafe dynamic ResizeArray(int length, Array array)
+        {
+            *(int*)Unsafe.AsPointer(ref Unsafe.As<RelaxedGrossObject>(array).Data) = length;
+            return array;
         }
 
         sealed unsafe class RelaxedGrossObject
