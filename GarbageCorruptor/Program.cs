b1790f1bb5af64e86a9b46d835302f3b4e95cf6b
cetio <qubbinyt@gmail.com>
2023-11-29 20:07:29 -0500

    Add project files.

---
+++ GarbageCorruptor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace GarbageCorruptor
+{
+    internal class Program
+    {
+        static unsafe void Main(string[] args)
+        {
+            int[] arr = new int[1];
+            *(int*)Unsafe.AsPointer(ref Unsafe.As<RelaxedGrossObject>(arr).Data) = int.MaxValue;
+            GC.Collect();
+        }
+
+        sealed unsafe class RelaxedGrossObject
+        {
+            public byte Data; // m_data
+        }
+    }
+}
